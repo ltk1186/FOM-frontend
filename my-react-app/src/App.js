5a590dda2a76f0ef1c10914492a746069ebee7b4
vairocanakara <ltk118698@gmail.com>
2025-05-20 17:06:06 +0900

    favicon, header update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,30 +1,40 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import Record from "./pages/Record";
+
 import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Counter from "./pages/Counter";
 import Signup from "./pages/Signup";
 import LoginIntro from "./pages/LoginIntro";
+import Counter from "./pages/Counter";
+import Record from "./pages/Record";
+import Home from "./pages/Home"; // Home 컴포넌트가 없으면 제거 가능
+
+import "./index.css"; // 전역 스타일 포함
 
 function App() {
-    return (
-        <div className="App">
-            <nav>
-                <Link to="/">홈</Link> | <Link to="/login">로그인</Link> |{" "}
-                <Link to="/counter">카운터</Link> |{" "}
-                <Link to="/record">일기</Link> | <Link to="/signup">회원가입</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<LoginIntro />} />        {/* 인트로 페이지 */}
-                <Route path="/home" element={<Home />} />          {/* 홈은 /home 으로 변경 */}
-                <Route path="/login" element={<Login />} />
-                <Route path="/counter" element={<Counter />} />
-                <Route path="/record" element={<Record />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      {/* 🌟 커스텀 헤더 */}
+      <header className="header">
+        <nav className="header-nav">
+          <Link to="/" className="nav-item">홈</Link>
+          <Link to="/login" className="nav-item">로그인</Link>
+          <Link to="/counter" className="nav-item">카운터</Link>
+          <Link to="/record" className="nav-item">일기</Link>
+          <Link to="/signup" className="nav-item">회원가입</Link>
+        </nav>
+      </header>
+
+      {/* 페이지 라우팅 */}
+      <Routes>
+        <Route path="/" element={<LoginIntro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/record" element={<Record />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} /> {/* 필요 시 */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
