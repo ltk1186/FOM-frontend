d7e8ff9a04cb34db4734f409639062a0a6d31b04
vairocanakara <ltk118698@gmail.com>
2025-05-20 14:55:41 +0900

    login, signup, index.css design update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { Routes, Route, Link } from "react-router-dom";
 import Record from "./pages/Record";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Counter from "./pages/Counter";
 import Signup from "./pages/Signup";
+import LoginIntro from "./pages/LoginIntro";
 
 function App() {
     return (
         <div className="App">
             <nav>
-                <Link to="/">Home</Link> | <Link to="/login">Login</Link> |{" "}
-                <Link to="/counter">Counter</Link> |{" "}
-                <Link to="/record">Record</Link> |{" "}
-                <Link to="/signup">Signup</Link> |{" "}
+                <Link to="/">홈</Link> | <Link to="/login">로그인</Link> |{" "}
+                <Link to="/counter">카운터</Link> |{" "}
+                <Link to="/record">일기</Link> | <Link to="/signup">회원가입</Link>
             </nav>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<LoginIntro />} />        {/* 인트로 페이지 */}
+                <Route path="/home" element={<Home />} />          {/* 홈은 /home 으로 변경 */}
                 <Route path="/login" element={<Login />} />
                 <Route path="/counter" element={<Counter />} />
                 <Route path="/record" element={<Record />} />
